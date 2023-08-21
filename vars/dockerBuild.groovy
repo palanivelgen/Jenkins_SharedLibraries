ae2fb44f31c0ebbaf6e941ac89a7338adca95e79
Palanivel Subbiah <palanivelgen@gmail.com>
2023-08-21 18:17:41 +0530

    updated docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,5 +3,6 @@ def call(String hubUser,String project, String imageTag){
     sh """
     docker image build -t ${hubUser}/${project} .
     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
