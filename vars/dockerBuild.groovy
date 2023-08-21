1f7baaad25a20a5cf1dd446af6d67819119bd02d
Palanivel Subbiah <palanivelgen@gmail.com>
2023-08-21 17:51:01 +0530

    build docker image

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,7 @@
+def call(String hubUser,String project, String imageTag){
+
+    sh """
+    docker image build -t ${hubUser}/${project}
+    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
+    """
+}
